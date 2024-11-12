5bf4ab8ccd4c4fe30ee1c8392ed7c86c57830ab0
Timothy Chai <159506546+tchai17@users.noreply.github.com>
2024-11-12 12:11:14 +0800

    Update to fit component props in frotnend

--- src/main/java/com/fdmgroup/backend_eventhub/livechat/models/ModuleAction.java
+++ src/main/java/com/fdmgroup/backend_eventhub/livechat/models/ModuleAction.java
@@ -2,5 +2,6 @@ package com.fdmgroup.backend_eventhub.livechat.models;
 
 import java.time.LocalDateTime;
 
-public record ModuleAction(String ID, String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP) {
+public record ModuleAction(String ID, String TYPE, String SESSION_ID, String SENDER, LocalDateTime TIMESTAMP,
+                           String TITLE, String CONTENT, String IMAGE_URL) {
 }
